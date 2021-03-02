aec7e64c2db693b942ef7e3eb523ca0f2e7fb95e
ericb2020 <ebrauer83@gmail.com>
2021-03-02 17:16:42 -0600

    created Member class with introduce method

--- lib/app.py
+++ lib/app.py
@@ -1,11 +1,11 @@
 # Write your code here!
 
 #PSEUDOCODE
-    # create a Member class with attributes:
-        #properties:
-            #full_name
-        #methods:
-            #introduce
+    # * create a Member class with attributes:
+        # * properties:
+            # * full_name
+        # * methods:
+            # * introduce
     # create childClass Student
         #properties:
             #reason
@@ -27,23 +27,40 @@
             #add_participant
             #print_details
     
+class Member:
+    def __init__(self, full_name):
+        self.name = full_name
+    def introduce(self):
+        return f"Hi, my name is {self.name}!"
+
+jane = Member("Jane Doe")
+
+print(jane.introduce())
+
+# workshop = Workshop("12/03/2014", "Shutl")
+
+# jane = Student("Jane Doe", "I am trying to learn programming and need some help")
+# lena = Student("Lena Smith", "I am really excited about learning to program!")
 
 
-workshop = Workshop("12/03/2014", "Shutl")
+# vicky = Instructor("Vicky Python", "I want to help people learn coding.")
+# vicky.add_skill("HTML")
+# vicky.add_skill("JavaScript")
 
-jane = Student("Jane Doe", "I am trying to learn programming and need some help")
-lena = Student("Lena Smith", "I am really excited about learning to program!")
-vicky = Instructor("Vicky Python", "I want to help people learn coding.")
-vicky.add_skill("HTML")
-vicky.add_skill("JavaScript")
-nicole = Instructor("Nicole McMillan", "I have been programming for 5 years in Python and want to spread the love")
-nicole.add_skill("Python")
 
-workshop.add_participant(jane)
-workshop.add_participant(lena)
-workshop.add_participant(vicky)
-workshop.add_participant(nicole)
-workshop.print_details()
+# nicole = Instructor("Nicole McMillan", "I have been programming for 5 years in Python and want to spread the love")
+# nicole.add_skill("Python")
+
+
+# workshop.add_participant(jane)
+# workshop.add_participant(lena)
+# workshop.add_participant(vicky)
+# workshop.add_participant(nicole)
+
+
+# workshop.print_details()
+
+
 # =>
 # Workshop - 12/03/2014 - Shutl
 #
