02a674edea1b641aaba6c3bf0b83ed1b1adc9e49
ericb2020 <ebrauer83@gmail.com>
2021-03-02 17:08:16 -0600

    first commit

--- lib/app.py
+++ lib/app.py
@@ -1,1 +1,59 @@
 # Write your code here!
+
+#PSEUDOCODE
+    # create a Member class with attributes:
+        #properties:
+            #full_name
+        #methods:
+            #introduce
+    # create childClass Student
+        #properties:
+            #reason
+        #methods:
+    # create childClass Instructor
+        #properties:
+            #bio
+            #skills
+        #methods:
+            #add_skill
+#Part II: Workshops
+    # create a Workshop class
+        #properties:
+            #date
+            #subject
+            #instructors
+            #students
+        #methods:
+            #add_participant
+            #print_details
+    
+
+
+workshop = Workshop("12/03/2014", "Shutl")
+
+jane = Student("Jane Doe", "I am trying to learn programming and need some help")
+lena = Student("Lena Smith", "I am really excited about learning to program!")
+vicky = Instructor("Vicky Python", "I want to help people learn coding.")
+vicky.add_skill("HTML")
+vicky.add_skill("JavaScript")
+nicole = Instructor("Nicole McMillan", "I have been programming for 5 years in Python and want to spread the love")
+nicole.add_skill("Python")
+
+workshop.add_participant(jane)
+workshop.add_participant(lena)
+workshop.add_participant(vicky)
+workshop.add_participant(nicole)
+workshop.print_details()
+# =>
+# Workshop - 12/03/2014 - Shutl
+#
+# Students
+# 1. Jane Doe - I am trying to learn programming and need some help
+# 2. Lena Smith - I am really excited about learning to program!
+#
+# Instructors
+# 1. Vicky Ruby - HTML, JavaScript
+#    I want to help people learn coding.
+# 2. Nicole McMillan - Ruby
+#    I have been programming for 5 years in Ruby and want to spread the love
+#
